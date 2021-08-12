a908c14ec95eeb7e3de62433671c6896add3921f
David Hecker <HECKERDW@GMAIL.COM>
2021-08-12 15:37:49 -0700

    css update + buttons

--- js/app.js
+++ js/app.js
@@ -1,29 +1,28 @@
 'use strict';
 
-let totalCorrect = 0;
 
 let userName = prompt("Hi! Welcome to my Code 201 week one project. What's your name??")
-alert("Hello there " + userName + ", lets get started with some more pop-up questions!")
+alert("Hello there " + userName + ", checkout the bottom of my page for questions! correct answers will increase your score!")
 
 function favoriteColor(){
   let likeColor = prompt("Do you think my favorite color is pink? (yes/no)")
   if (likeColor.toLowerCase() == "yes" || likeColor.toLowerCase() == "y") {
     alert("Pink is cool, but I really like purple!")
   } else if (likeColor.toLowerCase() == "no" || likeColor.toLowerCase() == "n") {
-    alert("Good guess! My favorite color is actually purple")
-    totalCorrect++;
+    alert("Good guess! My favorite color is actually purple");
+    incrementScore();
   } else {
     alert("I only speak Yes's and No's so...let's just move on.")
   }
   console.log(likeColor)
 }
-favoriteColor()
+
 
 function fearOfHeights(){
   let likeHeights = prompt("Do you think I am scared of heights? (yes/no)")
   if (likeHeights.toLowerCase() == "yes" || likeHeights.toLowerCase() == "y") {
     alert("Correct! I am terrified of heights.")
-    totalCorrect++;
+    incrementScore();
   } else if (likeHeights.toLowerCase() == "no" || likeHeights.toLowerCase() == "n") {
     alert("I wish. I am terrified of heights")
   } else {
@@ -31,13 +30,13 @@ function fearOfHeights(){
   }
   console.log(likeHeights)
 }
-fearOfHeights()
+
 
 function rockClimbing(){
   let likeCLimbing = prompt("Do you think I enjoy rock climbing? (yes/no)")
   if (likeCLimbing.toLowerCase() == "yes" || likeCLimbing.toLowerCase() == "y") {
     alert("Truth! Rock climbing is an awesome challenge and fantastic exercise")
-    totalCorrect++;
+    incrementScore();
   } else if (likeCLimbing.toLowerCase() == "no" || likeCLimbing.toLowerCase() == "n") {
     alert("Despite my fear of heights, I do infact love rock climbing!")
   } else {
@@ -45,13 +44,13 @@ function rockClimbing(){
   }
   console.log(likeCLimbing)
   }
-rockClimbing()
+
 
 function programmer(){
   let likeCode = prompt("Do you think I like JavaScript more than CSS? (yes/no)")
   if (likeCode.toLowerCase() == "yes" || likeCode.toLowerCase() == "y") {
     alert("Heck yes I do! I appreciate the logical thought required for JS")
-    totalCorrect++;
+    incrementScore();
   } else if (likeCode.toLowerCase() == "no" || likeCode.toLowerCase() == "n") {
     alert("I have no style, so CSS is not for me.")
   } else {
@@ -59,7 +58,6 @@ function programmer(){
   }
   console.log(likeCode)
 }
-programmer()
 
 function goNoGo(){
   let likeYesNo = prompt("Do you think I like yes or no questions? (yes/no)")
@@ -67,13 +65,13 @@ function goNoGo(){
     alert("nah, they're too absolute.")
   } else if (likeYesNo.toLowerCase() == "no" || likeYesNo.toLowerCase() == "n") {
     alert("yeah, they're not great for getting to know someone.")
-    totalCorrect++;
+    incrementScore();
   } else {
     alert("I only speak Yes's and No's so...let's just move on.")
   }
   console.log(likeYesNo)
   }
-goNoGo()
+
 
 
 // number guessing game
@@ -94,13 +92,13 @@ function randomNumber(){
       attempts++;
     } else {
       alert('Dang! Nice Guess! The correct answer WAS ' + correctNumber + '!!')
-      totalCorrect++;
+      incrementScore();
       break;
     }
   }
   alert('Nice guesses ' + userName + "! The correct answer was " + correctNumber);
   }
-randomNumber()
+
 
 // guess from an array game
 
@@ -114,7 +112,7 @@ function filmDirectors(){
     for (let i = 0; i < directors.length; i++) {
       if (directors[i] == directorGuess.toLowerCase()) {
         alert('FANTASTIC Guess!! I love ' + directorGuess)
-        totalCorrect++;
+        incrementScore();
         flag = false
       }
     }
@@ -126,7 +124,18 @@ function filmDirectors(){
   }
   alert('you could have guessed ' + directors + ' and been correct!')
 }
-filmDirectors()
 
 
-alert("Thank you for making it to the end! You got " + totalCorrect + "/7 questions right! Have a nice day " + userName + "!")
+
+// alert("Thank you for making it to the end! You got " + incrementScore() "/7 questions right! Have a nice day " + userName + "!")
+
+function incrementScore() {
+  let element = document.getElementById('score');
+  let value = element.innerHTML;
+  let num = parseInt(value);
+
+  num += 1;
+
+  console.log(num);
+  document.getElementById('score').innerHTML = num;
+}
